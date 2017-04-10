3ef9a1fc0e48a9d318e111c61549d0c6dd45961e
DutChen18 <DutChen18@gmail.com>
2017-04-10 19:33:53 +0200

    Added connection close

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
 _.I("_scripts/std.js");
+_.I("_scripts/file.js");
 _.I("_scripts/event.js");
 _.I("_scripts/websocket.js");
 
+var wsList = [];
+
 $ws.addProtocol("chat");
 $event.handler("ws_new", function(e) {
+	wsList.push(e.ws);
+	$.write("open");
 	e.ws.handler("message", function(ee) {
 		$.write(ee.message);
+	});
+	e.ws.handler("close", function(ee) {
+		wsList.splice(wsList.indexOf(e.ws), 1);
+		$.write("close " + ee.timeout);
 	});
 });
