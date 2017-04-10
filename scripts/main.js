4fefaa7db41d754d1a389073434bd39bc9ac509e
DutChen18 <DutChen18@gmail.com>
2017-04-10 21:07:15 +0200

    Lots of little changes
    Finished up ws protocol
    Finished up server js interaction
    Added server part of chat system
    Changed chat message protocol

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,23 @@ _.I("_scripts/file.js");
 _.I("_scripts/event.js");
 _.I("_scripts/websocket.js");
 
-var wsList = [];
+var chatList = [];
 
 $ws.addProtocol("chat");
 $event.handler("ws_new", function(e) {
-	wsList.push(e.ws);
-	$.write("open");
-	e.ws.handler("message", function(ee) {
-		$.write(ee.message);
-	});
-	e.ws.handler("close", function(ee) {
-		wsList.splice(wsList.indexOf(e.ws), 1);
-		$.write("close " + ee.timeout);
-	});
+	if (e.ws.protocol == "chat") {
+		chatList.push(e.ws);
+		e.ws.handler("message", function(ee) {
+			var file = $file.read("data/chat.txt");
+			file.push(ee.message);
+			if (file.length > 16)
+				file.splice(0, file.length - 16);
+			$file.write("data/chat.txt", file);
+			for (var i = 0; i < chatList.length; i++)
+				chatList[i].write(ee.message);
+		});
+		e.ws.handler("close", function(ee) {
+			chatList.splice(chatList.indexOf(e.ws), 1);
+		});
+	}
 });
