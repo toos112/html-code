4efa5f620a626a0e5b79c80ff4f413e4d06fe9bb
DutChen18 <DutChen18@gmail.com>
2017-04-11 14:56:50 +0200

    1

--- scripts/main.js
+++ scripts/main.js
@@ -5,23 +5,37 @@ _.I("_scripts/websocket.js");
 
 var chatList = [];
 
-$ws.addProtocol("chat");
-$event.handler("ws_new", function(e) {
-	if (e.ws.protocol == "chat") {
-		chatList.push(e.ws);
-		e.ws.handler("message", function(ee) {
-			ee.message = ee.message.replaceAll("<", "&lt;");
-			ee.message = ee.message.replaceAll(">", "&gt;");
+function ChatClient(ws) {
+	this._ws = ws;
+	this.username = "Anonymous";
+	
+	this._ws.handler("message", function(ee) {
+		ee.message = ee.message.replaceAll("<", "&lt;");
+		ee.message = ee.message.replaceAll(">", "&gt;");
+		
+		var prefix = ee.message.substr(0, 1);
+		var payload = ee.message.substr(1, ee.message.length - 1);
+		
+		if (prefix.startsWith("@"))
+			username = payload;
+		else if (prefix.startsWith(":")) {
 			var file = $file.read("data/chat.txt");
-			file.push(ee.message);
+			file.push(payload);
 			if (file.length > 16)
 				file.splice(0, file.length - 16);
 			$file.write("data/chat.txt", file);
 			for (var i = 0; i < chatList.length; i++)
-				chatList[i].write(ee.message);
-		});
-		e.ws.handler("close", function(ee) {
-			chatList.splice(chatList.indexOf(e.ws), 1);
-		});
+				chatList[i].write(payload);
+		}
+	});
+	this._ws.handler("close", function(ee) {
+		chatList.splice(chatList.indexOf(e.ws), 1);
+	});
+}
+
+$ws.addProtocol("chat");
+$event.handler("ws_new", function(e) {
+	if (e.ws.protocol == "chat") {
+		chatList.push(new ChatClient(e.ws));
 	}
 });
