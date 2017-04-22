d7b04f8dfad96d0d9a610b8ad23f9cc439c92a17
toos112 <toos112kp@gmail.com>
2017-04-22 17:09:11 +0200

    update

--- root/login.js
+++ root/login.js
@@ -39,7 +39,7 @@ function send() {
 	xmlhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
 			createCookie("UUID", this.responseText);
-			div.innerHTML = "<span class = \"center\" id = \"nameText\" style = \"width : calc(100% - 4px); color: #ccc;\">You have logged in.</span><br/><button type = \"button\" href = \"index.html\" id = \"button\" style = \"margin-top: 6px; width: 96px;\"><span>Chat</span></button>";
+			div.innerHTML = "<span class = \"center\" id = \"nameText\" style = \"width : calc(100% - 4px); color: #ccc;\">You have logged in.</span><br/><button onclick = \"location.href = \"index.html\";\" id = \"button\" style = \"margin-top: 6px; width: 96px;\"><span>Chat</span></button>";
 		}
 	}
 }
