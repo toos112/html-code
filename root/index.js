9a2bd7a6fc1028e2f3036793ab87ee7e5fb73858
toos112 <toos112kp@gmail.com>
2017-04-24 14:33:17 +0200

    Register

--- root/index.js
+++ root/index.js
@@ -5,7 +5,9 @@ check(function(success) {
 });
 
 function setLoggedIn() {
+	var title = document.getElementById("title");
 	var div = document.getElementById("nameInput");
+	title.innerHTML = "Home";
 	div.innerHTML = "\
 		<span class = \"center\" id = \"nameText\" style = \"width: 250px; color: #ccc;\">\
 			You have logged in.\
@@ -58,4 +60,81 @@ function logout() {
 	setCookie("user", "null");
 	setCookie("UUID", "null");
 	location.replace("index.html");
+}
+
+function register() {
+	var title = document.getElementById("title");
+	var div = document.getElementById("input");
+	title.innerHTML = "Register";
+	div.innerHTML = "\
+		<div id = \"errorPlace\">\
+		</div>\
+		<div class = \"center\" style = \"margin-top: 5px; width: calc(40% + 84px); padding: 2px; border : 2px solid #444;\">\
+			<span class = \"center\" style = \"width : 160px; color: #ccc;\">\
+				Username: \
+			</span>\
+			<input type = \"text\" id = \"name\" placeholder = \"Type your Username here\" style = \"width: calc(100% - 164px);\"/><br/>\
+			<span class = \"center\" id = \"nameText\" style = \"width : 160px; color: #ccc;\">\
+				Email: \
+			</span>\
+			<input type = \"text\" id = \"email\" placeholder = \"Type your Email here\" style = \"width: calc(100% - 164px);\"/><br/>\
+			<span class = \"center\" id = \"nameText\" style = \"width : 160px; color: #ccc;\">\
+				Password: \
+			</span>\
+			<input type = \"password\" id = \"password1\" placeholder = \"Type your Password here\" style = \"width: calc(100% - 164px);\"/><br/>\
+			<span class = \"center\" id = \"nameText\" style = \"width : 160px; color: #ccc;\">\
+				Repeat password: \
+			</span>\
+			<input type = \"password\" id = \"password2\" placeholder = \"Repeat your Password\" style = \"width: calc(100% - 164px);\"/><br/>\
+			<button onclick = \"home();\" id = \"button\" style = \"margin-top: 6px; width: 96px;\">\
+				<span>\
+					Cancel\
+				</span>\
+			</button>\
+			<button onclick = \"commit();\" id = \"button\" style = \"margin-top: 6px; width: 96px;\">\
+				<span>\
+					Commit\
+				</span>\
+			</button>\
+		</div>";
+}
+
+function wrongInput(text) {
+	var div = document.getElementById("errorPlace");
+	div.className += "center";
+	div.style.marginTop = "5px";
+	div.style.width = "calc(40% + 84px)";
+	div.style.padding = "2px";
+	div.style.border = "2px solid #444";
+	div.innerHTML = text;
+}
+
+function commit() {
+	var name = document.getElementById("name");
+	var email = document.getElementById("email");
+	var password1 = document.getElementById("password1");
+	var password2 = document.getElementById("password2");
+	if (name.length > 5  && name.length <= 16 && email.indexOf("@") != -1 && email.indexOf(".") > email.indexOf("@") && password1.length > 6 && password1.length <= 24 && password1.value == password2.value) {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "auth/register.js?user=" + name.value + "&email=" + email.value + "&password=" + password1.value, true);
+		xmlhttp.send();
+	} else if (name.value == "") {
+		wrongInput("Please enter your username");
+	} else if (name.length < 5) {
+		wrongInput("Please enter a longer username");
+	} else if (name.length >= 16) {
+		wrongInput("Please enter a shorter username");
+	} else if (email.indexOf("@") == -1) {
+		wrongInput("Please enter a  valid email");
+	} else if (email.indexOf(".") < email.indexOf("@")) {
+		wrongInput("Please enter a valid email");
+	} else if (password1.length < 6) {
+		wrongInput("Please enter a longer password");
+	} else if (password1.length >= 24) {
+		wrongInput("Please enter a shorter password");
+	} else if (password1.value == password2.value) {
+		wrongInput("Please repeat your password");
+	} else {
+		wrongInput("An error accoured.<br/>Please try again.");
+	}
 }
