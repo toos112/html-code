df3fa4f4737be209946d1d5ef84ed0bb835c87ce
DutChen18 <DutChen18@gmail.com>
2017-04-29 13:24:02 +0200

    fuck encryptie en github

--- scripts/login.js
+++ scripts/login.js
@@ -5,7 +5,7 @@ _.I("_scripts/encode.js");
 
 var _genID = function(length) {
 	var text = "";
-	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/-";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-";
 	for(var i = 0; i < length; i++)
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
 	return text;
