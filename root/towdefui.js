fdad999c770502c9c68567f33d24fb903eeaeecd
brondanboos <31004748+brondanboos@users.noreply.github.com>
2017-08-17 20:44:42 +0200

    ik haat iedereen

--- root/towdefui.js
+++ root/towdefui.js
@@ -6,4 +6,19 @@ var renderUI = function(ctx) {
 	ctx.fillText("Coins:" + coins,20,448); 
 	ctx.fillStyle = "#ff00ff";
 	ctx.fillText("Lives:" + lives,20,480);
+
+	ctx.rect(125,394,256,64);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(189,394);
+ctx.lineTo(189,458);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(253,394);
+ctx.lineTo(253,458);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(317,394);
+ctx.lineTo(317,458);
+ctx.stroke();
 };
