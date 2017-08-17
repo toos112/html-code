a5f9538b4c7f39035fec87a8b9ce5436ef21001f
brondanboos <31004748+brondanboos@users.noreply.github.com>
2017-08-17 20:56:34 +0200

    haai

--- root/towdefui.js
+++ root/towdefui.js
@@ -7,18 +7,18 @@ var renderUI = function(ctx) {
 	ctx.fillStyle = "#ff00ff";
 	ctx.fillText("Lives:" + lives,20,480);
 
+	ctx.beginPath();
 	ctx.rect(125,394,256,64);
-ctx.stroke();
-ctx.beginPath();
-ctx.moveTo(189,394);
-ctx.lineTo(189,458);
-ctx.stroke();
-ctx.beginPath();
-ctx.moveTo(253,394);
-ctx.lineTo(253,458);
-ctx.stroke();
-ctx.beginPath();
-ctx.moveTo(317,394);
-ctx.lineTo(317,458);
-ctx.stroke();
+	ctx.moveTo(189,394);
+	ctx.lineTo(189,458);
+	ctx.moveTo(253,394);
+	ctx.lineTo(253,458);
+	ctx.moveTo(317,394);
+	ctx.lineTo(317,458);
+	ctx.stroke();
+	
+	if (mouseIsTile) {
+		ctx.fillStyle = "#ffffff";
+		ctx.fillText("tilename:" + mouseTile.name,30,30);
+	}
 };
