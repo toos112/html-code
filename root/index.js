f48a40011034ad8a1519c01e3030ece9d6e051ce
brondanboos <31004748+brondanboos@users.noreply.github.com>
2018-01-09 21:14:00 +0100

    een van jullie twee kan niet typen

--- root/index.js
+++ root/index.js
@@ -141,41 +141,41 @@ function service() {
 			<span style = 'white-space: normal; overflow-y: scroll; height: 100%; width: 100%; text-align: left;'>\
 				Don't take these too seriously, this whole page (including this sentence) is an inside joke you probably don't understand.\
 				\
-				By creating a account you agree to all of the below.<br/>\
+				By creating a account you agree to all of the points below.<br/>\
 				<br/>\
 				You agree not abuse any bugs you may find. You agree to report any bugs you may find. You can do so using the chat function.<br/>\
 				<br/>\
-				You agree to respect the copyright which includes the ideas of the website and the website itself. It also includes the style and mecanics of the website.<br/>\
+				You agree to respect the copyright which includes the ideas of the website and the website itself. It also includes the style and mechanics of the website.<br/>\
 				<br/>\
 				You agree that your account may be terminated at any time of you do not obey the following rules.<br/>\
 				1.  You listen to the staff it is their free time aswell yes staff is not payed.<br/>\
-				2.  Do not use exesive language.<br/>\
-				3.  Do not treaten anybody on our website.<br/>\
-				4.  Do not treaten the website.<br/>\
+				2.  Do not use excessive language.<br/>\
+				3.  Do not threaten anybody on our website.<br/>\
+				4.  Do not threaten the website.<br/>\
 				5.  Do not spam anything.<br/>\
-				6.  Have respeczt for those who dare to use the chat.<br/>\
+				6.  Have respect for those who dare to use the chat.<br/>\
 				7.  Do not troll to hard it should be fun for everybody. This does mean trolling is allowed.<br/>\
 				8.  Do not advertise anything without proper premission.<br/>\
 				9.  In general the chat should be a English chat.<br/>\
-				10. No sexism or recism or any other discrimination.<br/>\
-				11. No evesions of the sentence that was made by staff<br/>\
-				12. Use commen sence before you say something.<br/>\
+				10. No sexism or racism or any other discrimination.<br/>\
+				11. No evasions of the sentence that was made by staff<br/>\
+				12. Use common sence before you say something.<br/>\
 				<br/>\
 				You agree to us using cookies to make the experience on our website better and safer.<br/>\
 				<br/>\
-				We are in our right to terminate or suspend acces to this website immediately. We will try to notify you if we do but we are not required to do so.<br/>\
+				We are in our right to terminate or suspend access to this website immediately. We will try to notify you if we do but we are not required to do so.<br/>\
 				<br/>\
-				Our server allows you to chat. Our website is not resonsible or in any way related with what is posted in the chat.<br/>\
+				Our server allows you to chat. Our website is not responsible or in any way related with what is posted in the chat.<br/>\
 				<br/>\
-				Our server may contain links to third-party web sites or services that are not owned or controlled by this website. This website has no control over, and assumes no responsibility for, the content,privacy policies, or practices of any third party web sites or services. You further acknowledge andagree that this website shall not be responsible or liable, directly or indirectly, for anydamage or loss caused or alleged to be caused by or in connection with use of or reliance on anysuch content, goods or services available on or through any such web sites or services.<br/>\
+				Our server may contain links to third-party web sites or services that are not owned or controlled by this website. This website has no control over, and assumes no responsibility for, the content,privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that this website shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.<br/>\
 				<br/>\
-				We do safe all the private information that you give us. This information is saved on our server. This information will never be used for any third-company and not for adverising either.<br/>\
+				We do safe all the private information that you give us. This information is saved on our server. This information will never be used for any third-company and not for advertising either.<br/>\
 				<br/>\
-				We are in our right to change the Terms of Serivece at any time. We will try to notify you if we do but we are not required to do so.<br/>\
+				We are in our right to change the Terms of Service at any time. We will try to notify you if we do but we are not required to do so.<br/>\
 				<br/>\
-				If you violate the Terms of Service at any time in any way we are in our right to ban your ip and/or termitate your account and/or press charges against you.<br/>\
+				If you violate the Terms of Service at any time in any way we are in our right to ban your ip and/or terminate your account and/or press charges against you.<br/>\
 				<br/>\
-				This website is owned by its creator. Brandon.\
+				This website is owned by its creators. Chen, Brandon and Gidion .\
 			</span>\
 		</div>\
 	</div>\
