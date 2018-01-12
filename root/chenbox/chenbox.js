6c706b67af05444486e0bb3fa2db9e972b03c25a
DutChen18 <DutChen18@gmail.com>
2018-01-12 15:19:28 +0100

    ziet er niet uit, iemand die niet chen heet moet css doen.

--- root/chenbox/chenbox.js
+++ root/chenbox/chenbox.js
@@ -1,6 +1,7 @@
 let USERNAME = null;
 let BODY = null;
 let CLIENT = null;
+let alert;
 
 let HTTP_GET = function(url, func) {
     let req;
@@ -16,6 +17,10 @@ let HTTP_GET = function(url, func) {
 
     req.open("GET", url, true)
     req.send();
+};
+
+let s2u = function(str) {
+	return str.split(" ").join("_");
 };
 
 let Client = function() {
@@ -39,6 +44,7 @@ let Client = function() {
         for (var i = 0; i < _msg.length; i++)
             msg[_msg[i].charAt(0)] = _msg[i].substr(1);
 
+		if (msg["/"] == "err") console.error("E" + msg["#"]);
 		if (msg["/"] == "ok" || msg["/"] == "err") {
 			let func = _this._Q.shift();
 			if (func) func(msg);
@@ -58,7 +64,46 @@ let setContent = function(url, func) {
 };
 
 let loadRooms = function() {
+	let roomList = document.getElementById("roomList");
+	let table = roomList.getElementsByTagName("table")[0];
+	let tbody = roomList.getElementsByTagName("tbody")[0];
+	tbody.innerHTML = "";
+	CLIENT._send("/rooms", function(msg) {
+		let ids = msg["*"].split(",");
+		if (ids[0] == "") ids = [];
+		for (let i = 0; i < ids.length; i++) {
+			tbody.innerHTML += "\
+			<tr>\
+				<td f='f32' id='rn#" + ids[i] + "'>loading...</td>\
+				<td id='rj#" + ids[i] + "'></td>\
+			</tr>";
+			CLIENT._send("/?room #" + ids[i], function(msg) {
+				let name = document.getElementById("rn#" + msg["#"]);
+				let join = document.getElementById("rj#" + msg["#"]);
+				name.innerHTML = msg["@"];
+				join.innerHTML = "<button f='f32' style='width: 100%;' onclick='joinRoom(" + msg["#"] + ");'>Join</button>";
+			});
+		}
+	});
+};
 
+let makeRoom = function() {
+	alert = new WindowAlert(240, 320);
+	HTTP_GET("makeRoom.html", function(html) {
+		alert.insert(toElemArray(html));
+	});
+	alert.open();
+};
+
+let actuallyMakeRoom = function() {
+	// ^^ best function name
+	let name = document.getElementById("mr#name").value;
+	CLIENT._send("/+room @" + s2u(name));
+	alert.close();
+};
+
+let joinRoom = function(id) {
+	CLIENT._send("/>room #" + id);
 };
 
 let login = function() {
